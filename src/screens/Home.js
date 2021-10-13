f20d46356799a34b1fddd5f4b8406971f5a2a2a2
ninhtao102 <ninh.tao.102@gmail.com>
2021-10-13 17:43:10 +0700

    change Home Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { FlatList, Image, View, Text, TouchableOpacity, TouchableHighlight, SafeAreaView, ScrollView } from 'react-native'
+import { FlatList, Image, ImageBackground, View, Text, TouchableOpacity, SafeAreaView, ScrollView} from 'react-native'
 import styles from '../styles/HomeStyles'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const bannerList = [
     {
@@ -12,7 +13,7 @@ const bannerList = [
     {
         id: 2,
         photo: 'https://feed.thecoffeehouse.com//content/images/2021/10/1200x1200.png',
-        title: 'cần món nước ngon, nhà & shoppeepay khao',
+        title: 'cần món nước ngon, shoppee khao',
         date: 'Đến 11/10',
     },
     {
@@ -42,7 +43,7 @@ const bannerList = [
     {
         id: 7,
         photo: 'https://feed.thecoffeehouse.com//content/images/2021/10/h-nh-m--b-n.jpg',
-        title: 'the coffee house sắp có phiên bản mới',
+        title: 'the coffee house có phiên bản mới',
         date: 'Đến 30/10',
     },
     {
@@ -53,34 +54,92 @@ const bannerList = [
     },
 ]
 
+const renderItem = ({ item }) => (
+    <View>
+        <TouchableOpacity>
+            <Image
+            style={styles.banner}
+            source={{ uri: item?.photo }}
+            />
+            <View>
+                <Text style={styles.title}>{item?.title}</Text>
+                <View style ={{flexDirection: 'row'}}>
+                    <Ionicons name="calendar-outline" size={16} color="#000" />
+                    <Text style={styles.date}>{item?.date}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    </View>
+);
+
 export default function Home() {
     return (
         <SafeAreaView>
             <ScrollView>
-                {/* <View>
-                    <TouchableHighlight>
-
-                    </TouchableHighlight>
-                </View> */}
-
                 <View>
-                    <View>
+                    <ImageBackground source={require('../images/backgroundContent.png')} resizeMode="repeat" style={{backgroundColor: '#e9d8a6',}}>
+                    <View style={styles.codeUser}>
                         <TouchableOpacity>
-                            <Text>Giao hàng</Text>
+                            <ImageBackground source={require('../images/backgroundCodeUser.png')} resizeMode="contain">
+                            <View style={styles.addPoint}>
+                                <Ionicons name="chevron-down-outline" size={20} color="#FFF" />
+                                <Text style={{color: '#FFF'}}>Tích điểm</Text>
+                            </View>
+                            <Text style={[styles.codeInfo, {fontSize: 18, fontWeight: 'bold',}]}>Ninh Tào</Text>
+                            <Text style={[styles.codeInfo,{marginBottom: 24}]}>68 BEAN</Text>
+                            <View style={styles.code}>
+                                <Image source={require('../images/barcode.gif')} style={styles.barcode} />
+                                <Text style={{textAlign: 'center', margin: 10}}>M161514512</Text>
+                            </View>
+                            </ImageBackground>
                         </TouchableOpacity>
                     </View>
-                    <Text style={styles.title}>Khám phá thêm </Text>
-                    <View style={{flexDirection: 'row'}}>
-                        <TouchableOpacity>
-                            <Text style={styles.btnDiscovery}>Ưu đãi đặc biệt</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text style={styles.btnDiscovery}>Cập nhật từ nhà</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text style={styles.btnDiscovery}>#CoffeeLover</Text>
-                        </TouchableOpacity>
+
+                    <View style={styles.content}>
+                        <View style={styles.option}>
+                            <TouchableOpacity>
+                            <Image
+                                style={styles.icon}
+                                source={require('../images/shipping_icon.jpg')}
+                            />
+                                <Text style={styles.txtOption}>Giao hàng</Text>
+                            </TouchableOpacity>
+                            <View style={styles.line}></View>
+                            <TouchableOpacity>
+                            <Image
+                                style={styles.icon}
+                                source={require('../images/bring_icon.jpg')}
+                            />
+                                <Text style={styles.txtOption}>Mang đi</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <Image
+                                style={styles.bannerImage}
+                                source={require('../images/BANNER-HOME-APP-CA-PHE-TAI-NHA.jpg')}
+                            />
+                        <Text style={styles.titleDiscovery}>Khám phá thêm </Text>
+                        <View style={{flexDirection: 'row'}}>
+                            <TouchableOpacity>
+                                <Text style={[styles.btnDiscovery, styles.activeBtnDiscovery]}>Ưu đãi đặc biệt</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <Text style={styles.btnDiscovery}>Cập nhật từ nhà</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <Text style={styles.btnDiscovery}>#CoffeeLover</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <FlatList
+                            data={bannerList}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.id}
+                            numColumns={2}
+                            horizontal={false}
+                            style={{ marginBottom: 100 }}
+                            columnWrapperStyle={{ flex: 1, justifyContent: "space-around" }}
+                        />
                     </View>
+                    </ImageBackground>
                 </View> 
             </ScrollView>
         </SafeAreaView>
--- src/styles/HomeStyles.js
+++ src/styles/HomeStyles.js
@@ -1,22 +1,120 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    title: {
+    codeUser: {
+        backgroundColor: '#ff9e00',
+        marginHorizontal: 16,
+        marginVertical: 16,
+        marginBottom: 100,
+        marginTop: 50,
+        paddingHorizontal: 8,
+        borderRadius: 20,
+    },
+    addPoint: {
+        width: 100,
+        padding: 8,
+        left: 200,
+        color: '#FFF',
+        backgroundColor: '#ff6d00',
+        borderWidth: 1,
+        borderTopWidth: 0,
+        borderColor: '#CCC',
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+        flexDirection: 'row',
+    },
+    codeInfo: {
+        color: '#FFF',
+        marginLeft: 8,
+    },
+    barcode: {
+        width: 220,
+        height: 40,
+        alignSelf: 'center',
+    },
+    code: {
+        backgroundColor: '#FFF',
+        height: 100,
+        padding: 16,
+        marginVertical: 16,
+        marginHorizontal: 8,
+        borderRadius: 10,
+    },
+    content: {
+        backgroundColor: '#FFF', borderTopLeftRadius: 16, borderTopRightRadius: 16,
+        shadowColor: "#000",
+        shadowOpacity: 1,
+        elevation: 24,
+    },
+    option: {
+        borderWidth: 1,
+        borderColor: '#CCC',
+        borderRadius: 20,
+        marginHorizontal: 16,
+        marginVertical: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    icon: {
+        width: 80,
+        height: 80
+    },
+    txtOption: {
+        color: '#000',
+        textAlign: 'center',
+        marginBottom: 16,
+    },
+    line: {
+        height: 80,
+        alignSelf: 'center',
+        borderRightWidth: 1,
+        borderRightColor: '#CCC',
+    },
+    bannerImage: {
+        width: 325,
+        height: 200, 
+        borderRadius: 20,
+        marginHorizontal: 16,
+        // shadowColor: "#000",
+        // shadowOpacity: 1,
+        // elevation: 24,
+    },
+    titleDiscovery: {
         color: '#000',
         fontWeight: 'bold',
-        marginHorizontal: 10,
-        marginVertical: 10,
+        marginTop: 48,
+        marginHorizontal: 16,
+        marginVertical: 8,
     },
-
     btnDiscovery: {
         marginVertical: 10,
         marginLeft: 10,
         padding: 8,
         width: 120,
         textAlign: 'center',
-        color: '#FF9900',
+        fontWeight: 'bold',
+        
+    },
+    activeBtnDiscovery: {
+        color: '#FF7900',
         backgroundColor: '#EEE',
         borderRadius: 20,
+    },
+    banner: {
+        width: 150,
+        height: 150,
+        borderRadius: 8,
+    },
+    title: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        marginVertical: 8,
+        color: '#000',
+        width: 150,
+    },
+    date: {
+        paddingLeft: 8,
+        marginBottom: 16,
     }
 })
 
