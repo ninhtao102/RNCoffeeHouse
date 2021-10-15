201ae1bfd1851876c5395666b6d65c2330ec5271
ninhtao102 <ninh.tao.102@gmail.com>
2021-10-15 08:46:37 +0700

    add Reactotron

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import Booking from './src/screens/Booking'
 import Home from './src/screens/Home'
 import StoreLocation from './src/screens/StoreLocation'
 import Voucher from './src/screens/Voucher'
+import reactotron from 'reactotron-react-native'
 
 export default function App() {
+
+  console.tron.log('aaaaaa')
+
   return (
     <View>
       {/* <Home/> */}
