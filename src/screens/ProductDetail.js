b08c0f8ac46208ab1d5e61eb0e0f2a6572f2bee1
ninhtao102 <ninh.tao.102@gmail.com>
2021-10-20 17:26:48 +0700

    create product detail screen

---
+++ src/screens/ProductDetail.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Image, View, Text, TextInput, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native'
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import styles from '../styles/ProductDetailStyles'
+
+
+
+export default function ProductDetail() {
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={{backgroundColor: '#EEE'}}>
+                    <View style={{backgroundColor: '#FFF'}}>
+                        <Image
+                            style={styles.productImages}
+                            source={{ uri: 'https://minio.thecoffeehouse.com/image/admin/caphe-suada--bacsiu_063797.jpg' }}
+                            />
+                        <View>
+                            <View style={{ padding: 16, flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <View>
+                                    <Text style={styles.productName}>Cà Phê Sữa Đá</Text>
+                                    <Text style={styles.price}>32.000đ</Text>
+                                </View>
+                                <Ionicons name="heart-outline" size={20} color="#000" style={styles.icon} />
+                            </View>
+                            <Text style={styles.productInfo}>
+                            Cà phê được pha phin truyền thống kết hợp với sữa đặc tạo nên hương vị đậm đà, hài hòa giữa vị ngọt đầu lưỡi và vị đắng thanh thoát nơi hậu vị.
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.option}>
+                        <Text style={styles.title}>Size</Text>
+                        <Text style={styles.desc}>Chọn 1 kích thước</Text>
+                    </View>
+
+                    <View style={styles.option}>
+                        <Text style={styles.title}>Yêu cầu khác</Text>
+                        <Text style={styles.desc}>Những tùy chọn khác</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Thêm ghi chú"
+                        />
+                    </View>
+
+                    <View style={[styles.option, {paddingTop: 0}]}>
+                        <View style={[styles.addMoreProduct, {marginHorizontal: 90}]}>
+                            <TouchableOpacity>
+                                <Text style={styles.btnAddMore}>-</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.addMoreValue}>1</Text>
+                            <TouchableOpacity>
+                                <Text style={styles.btnAddMore}>+</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <TouchableOpacity>
+                            <Text style={styles.addProduct}>Chọn sản phẩm</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
---
+++ src/styles/ProductDetailStyles.js
@@ -0,0 +1,84 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const { HEIGHT, WIDTH } = Dimensions.get('window');
+
+const btnGroup = {
+    margin: 8,
+    padding: 4,
+    width: 36,
+    height: 36,
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    borderRadius: 8,
+    backgroundColor: '#EEE'
+}
+
+const styles = StyleSheet.create({
+    productImages: {
+        width: WIDTH,
+        height: 360,
+        resizeMode: 'contain',
+    },
+    productName: {
+        color: '#000',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    price: {
+        fontWeight: 'bold'
+    },
+    icon: {
+        top: 12,
+        right: 12,
+    },
+    productInfo: {
+        paddingHorizontal: 16,
+        color: '#000',
+    },
+    title: {
+        fontSize: 16,
+        color: '#000',
+        fontWeight: 'bold',
+    },
+    option: {
+        marginTop: 8,
+        padding: 16,
+        backgroundColor: '#FFF'
+    },
+    desc: {
+        fontSize: 12,
+    },
+    input: {
+        marginTop: 16,
+        paddingLeft: 16,
+        height: 40,
+        borderWidth: 0.5,
+        borderRadius: 8,
+    },
+    addMoreProduct: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        
+    },
+    btnAddMore: {
+        ...btnGroup,
+
+    },
+    addMoreValue: {
+        ...btnGroup,
+        backgroundColor: '#FFF'
+    },
+    addProduct: {
+        padding: 12,
+        backgroundColor: '#FF6600',
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        borderRadius: 16,
+    }
+})
+
+export default styles;
